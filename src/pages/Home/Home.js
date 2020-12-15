b1244a1ccca7623e9114227d3a74c97952af2159
EDusik <eduardodusik@gmail.com>
2020-12-14 21:36:42 -0300

    feat: adding Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import About from '../../components/About/About';
+import Projects from '../../components/Projects/Projects';
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <About />
+      <Projects />
+    </React.Fragment>
+  );
+}
+
+export default Home;
