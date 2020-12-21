18c133581051ea97fc06bac0fe5230d885d870d6
EDusik <eduardodusik@gmail.com>
2020-12-21 09:48:42 -0300

    fix: solving bug undefine data About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -26,11 +26,11 @@ const About = () => {
     <Profile>
       <div className='profile'>
         <img type='image' data-test='avatar' src={state.profile.avatar_url} alt={state.profile.login} />
-        <h1>{state.profile.name}</h1>
-        <h2>{state.profile.company}</h2>
+        <h1>{state.profile.name ? state.profile.name : ""}</h1>
+        <h2>{state.profile.company ? state.profile.company : ""}</h2>
         <span data-test='profile-create-at'>Create at: {formatDate(state.profile.created_at)}</span>
         <span data-test='profile-updated-at'>Last update: {formatDate(state.profile.updated_at)}</span>
-        <p data-test='profile-bio'>{state.profile.bio}</p>
+        <p data-test='profile-bio'>{state.profile.bio ? state.profile.bio : ""}</p>
       </div>
     </Profile>
   );
