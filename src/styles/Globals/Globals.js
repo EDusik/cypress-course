97292553dc16b885d70f31d1b45450e5938d62ac
EDusik <eduardodusik@gmail.com>
2021-05-20 19:48:59 -0300

    refactor(fix): adding url google fonts

--- src/styles/Globals/Globals.js
+++ src/styles/Globals/Globals.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`  
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
